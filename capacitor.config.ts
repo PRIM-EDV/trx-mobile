92dbcee0485e8688b35d8aa810cfae48205b74dd
Lucas Mösch <lucasmoesch@live.de>
2024-08-16 19:26:11 +0200

    Run Ionic fixes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'trx-mobile',
-  webDir: 'www',
+  webDir: 'www/browser',
   server: {
     androidScheme: 'https'
   }
