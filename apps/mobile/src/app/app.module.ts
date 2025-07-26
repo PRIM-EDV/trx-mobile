3418848aed966ff6d5ab9c54d619415c26d86a19
Lucas Mösch <lucasmoesch@live.de>
2025-07-26 23:31:25 +0200

    Added preliminary rwork of bluetooth service

--- apps/mobile/src/app/app.module.ts
+++ apps/mobile/src/app/app.module.ts
@@ -4,12 +4,11 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { DeviceManagerComponent } from './device-manager/device-manager.component';
 import { PhNavigationBar } from './infrastructure/ui/navigation-bar/navigation-bar-component/navigation-bar.component';
 import { PhNavigationBarTarget } from './infrastructure/ui/navigation-bar/navigation-bar-target-component/navigation-bar-target.component';
 
 @NgModule({
-  declarations: [AppComponent, DeviceManagerComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule, 
     IonicModule.forRoot(), 
--- apps/mobile/src/app/device/device.component.ts
+++ apps/mobile/src/app/device/device.component.ts
@@ -1,16 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, effect } from '@angular/core';
 import { PhSwitch } from '@phobos/elements';
+import { BluetoothService } from '../infrastructure/bluetooth/bluetooth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-device',
     imports: [
+        CommonModule,
         PhSwitch
     ],
     templateUrl: './device.component.html',
     styleUrls: ['./device.component.scss']
 })
 export class DeviceComponent {
-    constructor() {
+
+    public bluetoothEnabledState: boolean = false;
+
+    bluetoothEnableChange = effect(() => {
+        this.bluetoothEnabledState = this.bluetooth.isEnabled();
+    });
+
+    constructor(public readonly bluetooth: BluetoothService) {
         // Initialization logic here
+    }
+
+    scan() {
+        this.bluetooth.scan();
+    }
+
+    changeBluetoothEnabledState() {
+        if (this.bluetooth.isEnabled()) {
+            this.bluetooth.disable();
+        } else {
+            this.bluetooth.enable();
+        }
     }
 }
